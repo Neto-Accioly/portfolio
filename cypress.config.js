b161d9694afa236833d5e6ebaf30049f2dd9be29
Neto Accioly <lindemiraccioly@gmail.com>
2025-08-22 17:11:19 -0300

    adicionando relatórios html com mochawesome e gravação de vídeos automaticos dos testes com cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     video: true,
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
